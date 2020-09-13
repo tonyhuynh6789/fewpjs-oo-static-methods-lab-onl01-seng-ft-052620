9e586919f93317641d165266ccd77842f525dcb1
tonyhuynh6789 <duc_thinh_03@yahoo.com>
2020-09-13 00:23:49 -0700

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 class Formatter {
   //add static methods here
+
+
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+
+
+  static sanitize(string){
+    return string.replace(/[^a-zA-Z0-9-' ]+/g, '')
+  }
+
+
+  static titleize(string){
+    const allowed = ["the", 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    const words = string.split(" ")
+    words[0] = this.capitalize(words[0]);
+    for(let i = 1; i < words.length; i++) {
+      if (!allowed.includes(words[i])){
+        words[i] = this.capitalize(words[i])
+      }
+    }
+    return words.join(" ")
+  }
 }
